7d9acb0e503088c53c89e33c934a1e4bdb7e14f5
Hieu Dao Le Duc <danhalisly@gmail.com>
2023-07-31 00:11:25 -0400

    Add user profile ui and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row pt-5">
+        <div class="col-3 px-5">
+            <img class="w-100 rounded-circle" src="https://avatars.githubusercontent.com/u/60619445?v=4" alt="Profile Picture" />
+        </div>
+        <div class="col-9">
+            <h3 class="mb-3">{{ $user->username }}</h3>
+            <div class="d-flex gap-5 mb-2">
+                <div><strong>0</strong> posts</div>
+                <div><strong>0</strong> followers</div>
+                <div><strong>0</strong> following</div>
             </div>
+            <div>
+                <p class="fw-bold">Hiếu Đào Lê Đức</p>
+                <p>🇻🇳 🇨🇦</p>
+                <a href="#" target="_blank">https://danhalis.github.io/portfolio/</a>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://raw.githubusercontent.com/danhalis/portfolio/master/public/about-photo.jpg" alt="" />
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://raw.githubusercontent.com/danhalis/portfolio/master/public/about-photo.jpg" alt="" />
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://raw.githubusercontent.com/danhalis/portfolio/master/public/about-photo.jpg" alt="" />
         </div>
     </div>
 </div>
